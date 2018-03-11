ba42a7d8fc1a31fff652019a4e9c675046c650ed
FLTTSHY\surva <survare@hotmail.com>
2018-03-11 23:00:00 +0200

    Adding splash screen

--- DynModels/main.cpp
+++ DynModels/main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
 #include <QDebug>
-
+#include <QSplashScreen>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Показ картинки загрузки
+    QSplashScreen splash(QPixmap(":/splashscreen/assets/splash.png"));
+    qDebug() << "Showing splash screen";
+    splash.show();
+
     MainWindow w;
+    splash.finish(&w);
     w.showMaximized();
 
     return a.exec();
