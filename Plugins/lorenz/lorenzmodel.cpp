18dbe867f611e2f0b319c30dab2469f63dfeb603
Alexey Rott <survare@hotmail.com>
2019-04-05 13:00:38 +0300

    added other methods for lorenz model

--- Plugins/lorenz/lorenzmodel.cpp
+++ Plugins/lorenz/lorenzmodel.cpp
@@ -43,7 +43,7 @@ QList<Element> LorenzModel::differentiate(const DiffSettings &settings)
         Element element;
         if (method == DiffMethod::Eilers)
         {
-//            x = prev_x + h*(r*prev_x*(1-prev_x/k));
+            //            x = prev_x + h*(r*prev_x*(1-prev_x/k));
             x = prev_x + h*(a*(prev_y-prev_x));
             y = prev_y + h*(b*prev_x-prev_y-prev_x*prev_z);
             z = prev_z + h*(-c*prev_z+prev_x*prev_y);
@@ -52,12 +52,20 @@ QList<Element> LorenzModel::differentiate(const DiffSettings &settings)
         {
             double K[2];
 
-//            // X
-//            K[0] = h * (r*prev_x*(1-prev_x/k));
-//            K[1] = h * (r*(prev_x+K[0])*(1-(prev_x+K[0])/k));
+            // X
+            K[0] = h*(a*(prev_y-prev_x));
+            K[0] = h*(a*((prev_y+K[0])-(prev_x+K[0])));
+            x = prev_x + (K[0]+K[1])/2.0;
 
-//            x = prev_x + (K[0]+K[1])/2.0;
+            // Y
+            K[0] = h * (b*prev_x-prev_y-prev_x*prev_z);
+            K[1] = h * (b*(prev_x+K[0])-(prev_y+K[0])-(prev_x+K[0])*(prev_z+K[0]));
+            y = prev_y + (K[0]+K[1])/2.0;
 
+            // Z
+            K[0] = h * (-c*prev_z+prev_x*prev_y);
+            K[1] = h * (-c*(prev_z+K[0])+(prev_x+K[0])*(prev_y+K[0]));
+            z = prev_z + (K[0]+K[1])/2.0;
         }
         // Для нахождения первых 5-х [0;5) точек для Адамса
         // Используется метод Р-К. Для избежания копипасты
@@ -66,15 +74,28 @@ QList<Element> LorenzModel::differentiate(const DiffSettings &settings)
         {
             if ((iteration < 5) || (method == DiffMethod::RungeKutta4thOrder))
             {
+                qreal K[4];
 
+                // X1
+                K[0] = h* (a*(prev_y-prev_x));
+                K[1] = h* (a*((prev_y+0.5*K[0])-(prev_x+0.5*K[0])));
+                K[2] = h* (a*((prev_y+0.5*K[1])-(prev_x+0.5*K[1])));
+                K[3] = h* (a*((prev_y+K[2])-(prev_x+K[2])));
+                x = prev_x + (K[0] + 2.0*K[1] + 2.0*K[2] + K[3])/6.0;
 
-//                qreal K[4];
+                // y
+                K[0] = h* (b*prev_x-prev_y-prev_x*prev_z);
+                K[1] = h* (b*(prev_x+0.5*K[0])-(prev_y+0.5*K[0])-(prev_x+0.5*K[0])*(prev_z+0.5*K[0]));
+                K[2] = h* (b*(prev_x+0.5*K[1])-(prev_y+0.5*K[1])-(prev_x+0.5*K[1])*(prev_z+0.5*K[1]));
+                K[3] = h* (b*(prev_x+K[2])-(prev_y+K[2])-(prev_x+K[2])*(prev_z+K[2]));
+                y = prev_y + (K[0] + 2.0*K[1] + 2.0*K[2] + K[3])/6.0;
 
-//                K[0] = h* (r*prev_x*(1-prev_x/k));
-//                K[1] = h* (r*(prev_x + K[0]/2.0)*(1-(prev_x + K[0]/2.0)/k));
-//                K[2] = h* (r*(prev_x + K[1]/2.0)*(1-(prev_x + K[1]/2.0)/k));
-//                K[3] = h* (r*(prev_x + K[2])*(1-(prev_x + K[2])/k));
-//                x = prev_x + ((K[0] + 2.0*K[1] + 2.0*K[2] + K[3]))/6.0;
+                // Z
+                K[0] = h* (-c*prev_z+prev_x*prev_y);
+                K[1] = h* (-c*(prev_z+0.5*K[0])+(prev_x+0.5*K[0])*(prev_y+0.5*K[0]));
+                K[2] = h* (-c*(prev_z+0.5*K[1])+(prev_x+0.5*K[1])*(prev_y+0.5*K[1]));
+                K[3] = h* (-c*(prev_z+K[2])+(prev_x+K[2])*(prev_y+K[2]));
+                z = prev_z + (K[0] + 2.0*K[1] + 2.0*K[2] + K[3])/6.0;
             }
         }
         if (method == DiffMethod::AdamsBashforth4rdOrder)
@@ -82,17 +103,39 @@ QList<Element> LorenzModel::differentiate(const DiffSettings &settings)
             // После просчета первых 5-х точек идет метод Адамса
             if (iteration >= 5)
             {
-//                qreal   x_5 = out.at(iteration-5).second.at(0), // Xn-5
-//                        x_4 = out.at(iteration-4).second.at(0), // Xn-4
-//                        x_3 = out.at(iteration-3).second.at(0), // Xn-3
-//                        x_2 = out.at(iteration-2).second.at(0), // Xn-2
-//                        x_1 = out.at(iteration-1).second.at(0);
+                qreal   x_5 = out.at(iteration-5).second.at(0), // X1n-5
+                        x_4 = out.at(iteration-4).second.at(0), // X1n-4
+                        x_3 = out.at(iteration-3).second.at(0), // X1n-3
+                        x_2 = out.at(iteration-2).second.at(0), // X1n-2
+                        x_1 = out.at(iteration-1).second.at(0), // X1n-1
+                        y_5 = out.at(iteration-5).second.at(1), // X2n-5
+                        y_4 = out.at(iteration-4).second.at(1), // X2n-4
+                        y_3 = out.at(iteration-3).second.at(1), // X2n-3
+                        y_2 = out.at(iteration-2).second.at(1), // X2n-2
+                        y_1 = out.at(iteration-1).second.at(1), // X2n-1
+                        z_5 = out.at(iteration-5).second.at(2),  // Yn-5
+                        z_4 = out.at(iteration-4).second.at(2),  // Yn-4
+                        z_3 = out.at(iteration-3).second.at(2),  // Yn-3
+                        z_2 = out.at(iteration-2).second.at(2),  // Yn-2
+                        z_1 = out.at(iteration-1).second.at(2);  // Yn-1
 
-//                x   = x_1 +h*((1901.0/720.0*(r*x_1*(1-x_1/k))) -
-//                              (1387.0/360.0*(r*x_1*(1-x_2/k))) +
-//                              (109.0/30.0*(r*x_1*(1-x_3/k)))  -
-//                              (637.0/360.0*(r*x_1*(1-x_4/k))) +
-//                              (251.0/720.0*(r*x_1*(1-x_5/k))));
+                x   = x_1+h*((1901.0/720.0*(a*(y_1-x_1))) -
+                             (1387.0/360.0 *(a*(y_2-x_2))) +
+                             (109.0/30.0   *(a*(y_3-x_3)))   -
+                             (637.0/360.0  *(a*(y_4-x_4)))  +
+                             (251.0/720.0  *(a*(y_5-x_5))));
+
+                y   = y_1+h*((1901.0/720.0*(b*x_1-y_1-x_1*z_1)) -
+                             (1387.0/360.0 *(b*x_2-y_2-x_2*z_2)) +
+                             (109.0/30.0   *(b*x_3-y_3-x_3*z_3))   -
+                             (637.0/360.0  *(b*x_4-y_4-x_4*z_4))  +
+                             (251.0/720.0  *(b*x_5-y_5-x_5*z_5)));
+
+                z   = z_1 +h*((1901.0/720.0 *(-c*z_1+x_1*y_1)) -
+                              (1387.0/360.0 *(-c*z_2+x_2*y_2)) +
+                              (109.0/30.0   *(-c*z_3+x_3*y_3)) -
+                              (637.0/360.0  *(-c*z_4+x_4*y_4)) +
+                              (251.0/720.0  *(-c*z_5+x_5*y_5)));
             }
         }
         prev_x = x;
